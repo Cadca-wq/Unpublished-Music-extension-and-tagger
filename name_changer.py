54a1cb42ab7e7a6fa4c766280c86514a6126c1e6
Christian <cadca06'gmail.com>
2026-07-14 10:04:06 +0000

    added test names for pathlib editing

--- name_changer.py
+++ name_changer.py
@@ -1,1 +1,16 @@
-print("hello")
+from pathlib import Path
+import os 
+
+def list_all_the_dir(directory):
+    ls = [x for x in directory.iterdir()]
+    print(ls)
+# this finds the path where name_changer.py sits, gets its parent dir and uses that to initialise the readme into a variable 
+Readme_path = Path(__file__).parent/ "README.md"
+
+anime_dir = Readme_path.parent.parent / "Durarara"
+# durarara is the folder i want to access
+print(anime_dir)
+if anime_dir.is_dir():
+    list_all_the_dir(anime_dir)
+
+list_all_the_dir(Readme_path.parent / "test_names")
